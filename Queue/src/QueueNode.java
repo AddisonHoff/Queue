059118f19aec53f46e1082df8b3d07e670292407
Addison <addisonghoff@gmail.com>
2020-11-18 07:48:02 -0700

    Implement QueueNode

--- Queue/src/QueueNode.java
+++ Queue/src/QueueNode.java
@@ -6,4 +6,50 @@ A Node class that holds data and points to a parent node.
 
 public class QueueNode <T> {
 
+    private T t;
+    private QueueNode parent;
+
+    //Constructors
+    public QueueNode() {
+
+    }
+
+    public QueueNode(T t) {
+        this.t = t;
+    }
+
+    public QueueNode(T t, QueueNode parent) {
+        this.t = t;
+        this.parent = parent;
+    }
+
+    //Getters and setters
+    public void setData(T t) {
+        this.t = t;
+    }
+
+    public T getData() {
+        return t;
+    }
+
+    //Parent
+    public void setParent(QueueNode p) {
+        this.parent = p;
+    }
+
+    public QueueNode getParent() {
+        return parent;
+    }
+
+
+
+    public String toString() {
+        return ("Node: " + t.toString());
+    }
+
+    public boolean equals(QueueNode x) {
+        return this.t.equals(x.t);
+    }
+
+
 }
