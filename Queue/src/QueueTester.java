dafe2b76d2545ab7429f1bc8f7da987e8f1f527f
Addison <addisonghoff@gmail.com>
2020-11-18 07:54:43 -0700

    Implement QueueTester

--- Queue/src/QueueTester.java
+++ Queue/src/QueueTester.java
@@ -4,5 +4,51 @@ Addison Hoff
 Tests function of Queue.java.
 */
 class QueueTester {
+    public static void main(String[] args) {
+        Queue testQueue = new Queue();
 
+
+
+        //Expected: true
+        System.out.println(testQueue.isEmpty());
+
+        testQueue.enqueue(1);
+
+        //Expected: false
+        System.out.println(testQueue.isEmpty());
+
+        testQueue.enqueue(10);
+        testQueue.enqueue(32);
+
+        //Expected: 1
+        System.out.println(testQueue.peek());
+
+        //Expected: 3
+        System.out.println(testQueue.size());
+
+        //Expected: 1
+        System.out.println(testQueue.dequeue());
+
+        //Expected: 2
+        System.out.println(testQueue.size());
+
+        //Expected: 10
+        System.out.println(testQueue.dequeue());
+
+        //Expected: 1
+        System.out.println(testQueue.size());
+
+        //Expected: 32
+        System.out.println(testQueue.dequeue());
+
+        //Expected: 0
+        System.out.println(testQueue.size());
+
+
+
+
+
+
+
+    }
 }
